5907ab4ee2f870975f45cc0f96231b71c6ddf811
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-04-08 17:52:00 -0300

    Update Gerando.py

--- Gerando.py
+++ Gerando.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 import numpy as np
-import cv2
+import cv2 misael
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import json
